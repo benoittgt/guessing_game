cd805f847d11fd532ca0aa8d1c9810f2ee5df913
Benoit Tigeot <benoit@hopsandfork.com>
2016-01-03 15:48:17 +0100

    Add better loop support
    Waiting for emojis now

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
 extern crate rand;
+// extern crate emojicons;
 
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+// use emojicons::Emojify;
 
 fn main() {
     println!("Deviner le nombre!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Le nombre secret est : {}", secret_number);
+    // println!("Le nombre secret est : {}", secret_number);
 
-    println!("Merci d'insérer le nombre.");
+    loop {
+        println!("Merci d'insérer le nombre.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Impossible de lire la ligne");
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Impossible de lire la ligne");
 
-    let guess: u32 = guess.trim().parse()
-        .ok()
-        .expect("Merci de rentrer un numéro");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
 
-    println!("Vous avez trouvé: {}", guess);
+        println!("Vous avez trouvé: {} ", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Trop petit!"),
-        Ordering::Greater => println!("Trop grand!"),
-        Ordering::Equal   => println!("BIMMMM"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Trop petit!"),
+            Ordering::Greater => println!("Trop grand!"),
+            Ordering::Equal   => {
+                println!("BIMMMM");
+                break;
+            }
+        }
     }
 }
